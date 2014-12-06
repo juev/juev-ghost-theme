e258075ac66f3f7b92ae002871d4f5f827d86f17
Evsyukov Denis <denis@evsyukov.org>
2014-12-06 20:03:26 +0400

    Adding grunt task for minify css and js

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // Task configuration.
+    clean: {
+      src: ['assets/js/', 'assets/css/']
+    },
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true
+      },
+      dist: {
+        src: ['source/scripts/shadowbox.js', 'source/scripts/app.js'],
+        dest: 'assets/js/app.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: ''
+      },
+      dist: {
+        files: {
+          'assets/js/app-min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed',
+          compass: true,
+          sourcemap: 'none'
+        },
+        files: {                         // Dictionary of files
+          'assets/css/main.css': 'source/sass/main.scss',       // 'destination': 'source'
+          'assets/css/fonts.css': 'source/sass/fonts.scss'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'sass', 'concat', 'uglify']);
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+Required ruby and compass.
+
+Init and compile source file:
+
+    $ npm install
+    $ grunt
